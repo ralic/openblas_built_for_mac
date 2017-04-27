abfc1b9d10917b87c730ec3d2e25e1adcdf6ca49
Ralic Lo <raliclo@ralics-mac-pro.attlocal.net>
2017-04-26 22:45:33 -0700

    Add Built libirary for mac users, assuming 8 threads, it was only for Windows

---
+++ install_lib_20170426/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.20.dev")
+SET(OpenBLAS_INCLUDE_DIRS /Users/raliclo/openblas/2017-04-26/include)
+SET(OpenBLAS_LIBRARIES /Users/raliclo/openblas/2017-04-26/lib/libopenblas.dylib)
